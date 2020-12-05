f1da9630f909f336747093103463e075172380f3
Danil Sotskov <Reddan61@yandex.ru>
2020-12-05 20:24:20 +0300

    Updates

---
+++ precache-manifest.c0d5829348150c5c57823af8c52e50ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d212a37bd8e9514b60a7b65a999471c2",
+    "url": "/TODO/index.html"
+  },
+  {
+    "revision": "f8ec58443f955bdafdd7",
+    "url": "/TODO/static/css/main.153cc2e4.chunk.css"
+  },
+  {
+    "revision": "9948d9a288936bfdb2b4",
+    "url": "/TODO/static/js/2.505eeedd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TODO/static/js/2.505eeedd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ec58443f955bdafdd7",
+    "url": "/TODO/static/js/main.495ad874.chunk.js"
+  },
+  {
+    "revision": "80950d686167e6d77641",
+    "url": "/TODO/static/js/runtime-main.5dd1603b.js"
+  }
+]);
